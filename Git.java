1608f7d444386e53a2ea91d3f2db08fe7d25f26a
skywater9 <demirgks09@gmail.com>
2026-09-24 14:01:16 -0700

    Updated README for hash()

--- Git.java
+++ Git.java
@@ -1,10 +1,8 @@
 import java.io.*;
-
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
 import java.util.HexFormat;
 
 public class Git {
@@ -32,7 +30,7 @@ public class Git {
         }
     }
 
-    //
+    // Hashes file contents using SHA-1
     public String hashFile(String filePath) {
         try {
             String fileContents = Files.readString(Path.of(filePath));
--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Git Project:
 
 Methods and Functions:
 - init() --- initializes folder structure: git/, objects/, HEAD, Index.
+- hash(String filePath) --- Hashes file contents using SHA-1
